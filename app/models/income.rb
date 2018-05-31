79eb67056edec94a428a9c5f54eed5d916b8b786
Ararat777 <danielpogosyan18@gmail.com>
2018-05-31 17:18:35 +0300

    last

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,21 +1,11 @@
 require "prawn"
 class Income < ApplicationRecord
   belongs_to :cash_box
-  after_create :make_income,:generate_pdf
+  after_create :make_income
   
   private
   
   def make_income
     self.cash_box.increase_cash_box_amount(self.amount)
-  end
-  
-  def generate_pdf
-    income = self
-    Prawn::Document.generate("#{Rails.root}/app/documents/incomes/#{income.id.to_s}.pdf") do
-       text "#{income.id}"
-       text "#{income.service}"
-       text "#{income.client}"
-       text "#{income.amount}"
-    end
   end
 end
